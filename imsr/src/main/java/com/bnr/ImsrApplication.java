685b0ebfa7a9a07c0216ec9da0042e20fa74d104
Naveen <naveenb216@gmail.com>
2017-07-05 13:02:55 +0530

    c2

---
+++ imsr/src/main/java/com/bnr/ImsrApplication.java
@@ -0,0 +1,12 @@
+package com.bnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsrApplication.class, args);
+	}
+}
